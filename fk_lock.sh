897b2b0952cd51e2458ab3de753294ba8145979b
Raphael <rparodi@student.42.fr>
2023-10-31 01:17:15 +0200

    Final Push

--- fk_lock.sh
+++ fk_lock.sh
@@ -3,6 +3,6 @@
 pkill firefox
 pkill Discord
 
-scrot ftlock.png
+gnome-screenshot -f ./ft_lock.png
 
-sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ./ftlock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ./ft_lock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
