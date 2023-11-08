e419d7debffddb2d5f23c34a91ac850e6ef1ec65
Raphaël <35407363+EniumRaphael@users.noreply.github.com>
2023-11-08 14:23:32 +0100

    Add files via upload

--- fk_lock.sh
+++ fk_lock.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -f ./*.png
 
-pkill firefox
-pkill Discord
+discord
+xdotool key Super+h
+sleep 0.2
 
-gnome-screenshot -f ./ft_lock.png
+import -window root -resize $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)x$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2) -delay 200 ~/Documents/Stuff/ft_locktroll-main/ft_lock.png
 
-sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ./ft_lock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ~/Documents/Stuff/ft_locktroll-main/ft_lock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
