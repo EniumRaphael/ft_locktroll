7360be3fc4faf1f90bab0039d39b0ce642389a44
Raphaël <35407363+EniumRaphael@users.noreply.github.com>
2023-11-15 12:18:35 +0100

    Update ft_lock.sh
    Now u don't have to edit anything

--- ft_lock.sh
+++ ft_lock.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-rm -f ./*.png
+rm -f ~/.ft_lock.png
 
 if ps -ef | grep -v grep | grep -q discord; then
 	discord
-  sleep 0.1
+ sleep 0.1
 	xdotool key Super+h
 	sleep 0.2
 fi
 
-import -window root -resize $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)x$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2) -delay 200 ~/Documents/Stuff/ft_locktroll-main/ft_lock.png
+import -window root -resize $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)x$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2) -delay 200 ~/.ft_lock.png
 
-/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ~/Documents/Stuff/ft_locktroll-main/ft_lock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+/sgoinfre/goinfre/Perso/jmaia/Public/pimp_my_lock_v2/pimp_my_lock ~/.ft_lock.png 0 0 $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1) $(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
